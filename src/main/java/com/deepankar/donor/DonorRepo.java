4b654bf71254921c4b3cd55f4fb77549c794ae00
dee2804 <aroradeepankar2804@gmail.com>
2020-01-06 14:53:11 +0530

    creation of donor repository

---
+++ src/main/java/com/deepankar/donor/DonorRepo.java
@@ -0,0 +1,10 @@
+package com.deepankar.donor;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.deepankar.donor.Donor;
+
+@Repository
+	public interface DonorRepo extends JpaRepository<Donor, Long>{
+
+	}
