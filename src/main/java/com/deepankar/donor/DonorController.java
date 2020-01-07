9458d54c35535855793830443a86a9394cc13c9f
dee2804 <aroradeepankar2804@gmail.com>
2020-01-07 16:23:57 +0530

    added donor controller

---
+++ src/main/java/com/deepankar/donor/DonorController.java
@@ -0,0 +1,27 @@
+package com.deepankar.donor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bloodBank")
+public class DonorController {
+
+    @Autowired
+    private DonorRepo donorRepository;
+    
+    @RequestMapping(value = "addDonor", method = RequestMethod.POST)
+    public ResponseEntity addDonor(Donor newDonor) {
+    	
+    	if(donorRepository.findByDonor(newDonor))
+    	{
+    		return new ResponseEntity<String>("Donor "+newDonor.getName()+" already exists", HttpStatus.CONFLICT);
+    	}
+        return new ResponseEntity<Donor>(newDonor, HttpStatus.OK);
+    }
+    
+}
