a50c0c0041c4b61576cf60e8e994ddaf1f9d5dd6
ranbir <ranbirsingh2804@gmail.com>
2020-01-10 02:27:54 +0530

    added more attributes to donor

--- src/main/java/com/deepankar/donor/Donor.java
+++ src/main/java/com/deepankar/donor/Donor.java
@@ -11,10 +11,18 @@ public class Donor {
 	private long id;
 	@Column(name = "name")
 	private String name;
-	@Column(name = "phoneNumber")
-	private int phoneNumber;
 	@Column(name = "bloodGroup")
 	private String bloodGroup;
+	@Column(name = "age")
+	private int age;
+	@Column(name = "gender")
+	private String gender;
+	@Column(name = "weight")
+	private int weight;
+	@Column(name = "phoneNumber")
+	private int phoneNumber;
+	@Column(name = "locality")
+	private String locality;
 	@Column(name = "city")
 	private String city;
 	@Column(name = "state")
@@ -22,66 +30,113 @@ public class Donor {
 	@Column(name = "country")
 	private String country;
 	
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getPhoneNumber() {
-		return phoneNumber;
-	}
-	public void setPhoneNumber(int phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-	public String getBloodGroup() {
-		return bloodGroup;
-	}
-	public void setBloodGroup(String bloodGroup) {
-		this.bloodGroup = bloodGroup;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public String getState() {
-		return state;
-	}
-	public void setState(String state) {
-		this.state = state;
-	}
-	public String getCountry() {
-		return country;
-	}
-	public void setCountry(String country) {
-		this.country = country;
-	}
-	
-	
-	public Donor(long id, String name, int phoneNumber, String bloodGroup, String city, String state, String country) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.phoneNumber = phoneNumber;
-		this.bloodGroup = bloodGroup;
-		this.city = city;
-		this.state = state;
-		this.country = country;
-	}
-	
 	public Donor() {
 		super();
 	}
-	
-	
+
+	public Donor(long id, String name, String bloodGroup, int age, String gender, int weight, int phoneNumber,
+			String locality, String city, String state, String country) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.bloodGroup = bloodGroup;
+		this.age = age;
+		this.gender = gender;
+		this.weight = weight;
+		this.phoneNumber = phoneNumber;
+		this.locality = locality;
+		this.city = city;
+		this.state = state;
+		this.country = country;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBloodGroup() {
+		return bloodGroup;
+	}
+
+	public void setBloodGroup(String bloodGroup) {
+		this.bloodGroup = bloodGroup;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getLocality() {
+		return locality;
+	}
+
+	public void setLocality(String locality) {
+		this.locality = locality;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
 	
 	//Setters, getters and constructors
 	
